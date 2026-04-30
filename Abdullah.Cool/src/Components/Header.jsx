43e9d57fb291e34919603fb9ca9e9e53820b0319
Mirza Khalil <mirzakhalilulrehman123456@gamil.com>
2026-04-30 21:28:18 +0500

    STYLE THE NAVBAR

--- Abdullah.Cool/src/Components/Header.jsx
+++ Abdullah.Cool/src/Components/Header.jsx
@@ -1,24 +1,26 @@
-import logo from '../assets/images/logo.png';
+import { IoCall } from "react-icons/io5";   // Ionic style
+import logo from "../assets/images/logo.png";
 function Header() {
-    return (
-        <header className="header">
-            <div className="logo">
-                <img src={logo} alt="Abdullah.cool Logo" />
-            </div>
-            <div className="links">
-                <a href="#home">Home</a>
-                <a href="#tech-stack">Tech Stack</a>
-                <a href="#projects">Projects</a>
-                <a href="#services">Services</a>
-                <a href="#about">About</a>
-                <a href="#process">Process</a>
-                <a href="#testimonials">Testimonials</a>
-            </div>
-            <div className="btn">
-                <button>Contact Me</button>
-            </div>
+  return (
+    <header className="header">
+    
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="links">
+          <a href="#home">Home</a>
+          <a href="#tech-stack">Tech Stack</a>
+          <a href="#projects">Projects</a>
+          <a href="#services">Services</a>
+          <a href="#about">About</a>
+          <a href="#process">Process</a>
+          <a href="#testimonials">Testimonials</a>
+        </div>
+        <div className="btn">
+          <button>Contact Me <span className="icon-box"><IoCall /></span></button>
+        </div>
 
-        </header>
-    )
+    </header>
+  );
 }
 export default Header;
